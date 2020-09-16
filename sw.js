bc7c444be95565546728401b0dfd4b7551ba2c3b
xprilion <xprilion@gmail.com>
2020-09-16 19:14:37 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,10 +27,10 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-816e53339681e05ab9ed.js"
+    "url": "webpack-runtime-49abf870e65bc8aeb7f9.js"
   },
   {
-    "url": "styles.241d19899ae927ceb62c.css"
+    "url": "styles.84745404fc326c5aac59.css"
   },
   {
     "url": "styles-89fd2ae28bdf06750a71.js"
@@ -39,11 +39,11 @@ self.__precacheManifest = [
     "url": "framework-a7bb16c5c6afeed647b2.js"
   },
   {
-    "url": "app-f1884878d92b5b33c1be.js"
+    "url": "app-6e6960e1fade86e416d8.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e58760e0d5f3d8d96b3938fa98cd3778"
+    "revision": "3eaa7196ad03d30790ae265e11e7b2ad"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-e5cb9e0c77a823b86dc2.js"
@@ -148,7 +148,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-f1884878d92b5b33c1be.js`))) {
+  if (!resources || !(await caches.match(`/app-6e6960e1fade86e416d8.js`))) {
     return await fetch(event.request)
   }
 
